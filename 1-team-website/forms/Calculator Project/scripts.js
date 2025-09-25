702d303609261a381da0a1249aa5970a07a69730
Will <wdavidson27@hanoverstudents.org>
2025-09-25 12:45:53 -0400

    new stuff

--- 1-team-website/forms/Calculator Project/scripts.js
+++ 1-team-website/forms/Calculator Project/scripts.js
@@ -1,10 +1,17 @@
 function calculateVelocity() {
     
+    var displacement = $('#displacement').val();
+    var time = $('#time').val();
 
 
+    var velocity = displacement / time;
 
+    $('#velocity').html('Velocity is: ' + velocity);
 
+}
 
+function calculateSurfaceArea() {
 
-
+    var baselength = $('baseLength').val();
+    var basewidth = $('baseWidth').val();
 }
