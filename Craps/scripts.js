f98675e4f8d316d25c918bba52c9af8abb36c838
Will <wdavidson27@hanoverstudents.org>
2025-10-09 12:46:11 -0400

    new thing

--- Craps/scripts.js
+++ Craps/scripts.js
@@ -1,47 +1,81 @@
-var point = 0;
+const MINIMUM_BET = 5;
+const STARTING_FUNDS = 50;
+
+let point = 0;
+let bet = 0;
+let winnings = STARTING_FUNDS;
 
 /*
  * Checks the result of the current roll and declare a win, loss, or continuation.
  */
 function checkRoll(roll) {
-    var message = "";
-
     if (point == 0) { // New round
         if (roll == 7 || roll == 11) {
-            $("#message").text("You win!");
+            endRound(true);
         } else if (roll == 2 || roll == 3 || roll == 12) {
-            $("#message").text("You lose!");
+            endRound(false);
         } else {
             $("#point").text(roll);
             point = roll;
         }
     } else { // Existing round
         if (roll == point) {
-            $("#message").text("You win!");
-            point = 0;
+            endRound(true);
         } else if (roll == 7) {
-            $("#message").text("You lose!");
-            point = 0;
+            endRound(false);
         }
     }
+}
+
+/*
+ * Ends the current round of play by either adding/removing the bet to/from the winnings. Also
+ * resets game controls to start another round.
+ *
+ * win - true if the round ended in a win, false otherwise
+ */
+function endRound(win) {
+    // Step 1: Update winnings
+    if (win) {
+        $("#message").text("You win!");
+        winnings += bet;
+    } else {
+        $("#message").text("You lose!");
+        winnings -= bet;
+    }
+
+    console.log("Winnings: " + winnings);
+
+    // Step 2: Reset game controls for another round
+    $("#point").text("X");
+    $("#bet").val("");
+    $("#bet").prop("disabled", false);
+    $("#winnings").text("$" + winnings);
+
+    bet = 0;
+    point = 0;
 }
 
 /*
  * Rolls both dice at the same time and checks the results.
  */
 function rollDice() {
-    if (point == 0) {
-        $("#point").text("X");
+    if (point > 0 || validateBet()) {
+        $("#message").text("");
+
+        let roll1 = rollDie("d1");
+        let roll2 = rollDie("d2");
+        let total = roll1 + roll2;
+
+        console.log("Total: " + total);
+
+        checkRoll(total);
+    } else {
+        if (winnings < MINIMUM_BET) {
+            $("#message").text("You don't have enough money to play");
+        } else  {
+            $("#message").text("Enter a bet between $" + MINIMUM_BET + " and $" + winnings);
+        }
     }
-    $("#message").text("");
-
-    var roll1 = rollDie("d1");
-    var roll2 = rollDie("d2");
-    var total = roll1 + roll2;
-
-    console.log("Total: " + total);
-
-    checkRoll(total);
 }
 
 /*
@@ -54,7 +88,7 @@ function rollDie(dieNum) {
     $("#" + dieNum + " ~ .pip").css("visibility", "hidden");
 
     // Step 2: generate a random number between 1 and 6 (inclusive)
-    var roll = Math.ceil(Math.random() * 6);
+    let roll = Math.ceil(Math.random() * 6);
     console.log(dieNum + ": " + roll);
 
     // Step 3: show the appropriate pips based on the roll
@@ -88,4 +122,27 @@ function rollDie(dieNum) {
     }
 
     return roll;
+}
+
+/*
+ * This function does two things:
+ *
+ * 1. Verifies that a valid bet has been entered (a valid bet must be greater than $5 but less than
+ *    or equal to the total winnings)
+ *
+ * 2. Disables the betting controls to prevent the bet from being changed mid-round
+ *
+ * If the bet is validated then true is returned, false otherwise.
+ */
+function validateBet() {
+    bet = parseInt($("#bet").val());
+
+    console.log("Bet: " + bet);
+
+    if (isNaN(bet) || bet < MINIMUM_BET || bet > winnings) {
+        return false;
+    } else {
+        $("#bet").prop("disabled", true);
+        return true;
+    }
 }
--- forms/javascript/scripts.js
+++ forms/javascript/scripts.js
@@ -13,3 +13,35 @@ function testArray() {
     $('#average').html('Your average is: ' + average);
     
 }
+
+function Card(suit, rank, value) {
+    this.suit = suit;
+    this.rank = rank;
+    this.value = value;
+}
+
+let deck = [];
+
+function shuffleDeck() {
+    let aceOfSpades = new Card("Spades", "A", 1);
+
+    console.log(aceOfSpades)
+    console.log("You were dealt the " + aceOfSpades.rank + " of " + aceOfSpades.suit);
+
+    let suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+
+    for(let i = 0; i < 4; i++) {
+        for (let rank = 1; rank < 14; rank++) {
+        console.log(rank + " of " + suits[i]);
+
+        let card = new Card(suits[i], rank, rank);
+        deck.push(card);
+        }
+    }
+    console.log(deck);
+}
+
+function dealCard() {
+   let nextCard = deck.pop();
+   $("#card").html(nextCard);
+}
