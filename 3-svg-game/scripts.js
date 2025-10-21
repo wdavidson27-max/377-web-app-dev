d15e558e7e22fc1ffd4b2ce5f6093b0becba15ea
Will <wdavidson27@hanoverstudents.org>
2025-10-21 12:46:42 -0400

    added alt colors

--- 3-svg-game/scripts.js
+++ 3-svg-game/scripts.js
@@ -1,4 +1,29 @@
+var turn = false;
+
+
+
 function dropChip(column) {
-    $('#c' + column + 'r5').attr("fill", "red");
-    
+    for (let row = 5; row > -1; row --) {
+        
+        console.log(row);
+        var currentSpot = $('#c' + column + 'r' + row).attr("fill");
+        console.log(currentSpot);
+        if (currentSpot == 'white') {
+            if (turn) {
+                $('#c' + column + 'r' + row).attr("fill", "red");
+                turn = false;
+                break;
+            } else {
+                $('#c' + column + 'r' + row).attr("fill", "yellow");
+                turn = true;
+                break;
+            }
+            
+            
+        }
+    }
+
 }
+
+chips = []
+
