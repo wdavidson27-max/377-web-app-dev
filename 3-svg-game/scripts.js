099e301d1e38fd961adccdff5a94c1693ad48bcf
Will <wdavidson27@hanoverstudents.org>
2025-10-22 12:49:56 -0400

    added wins

--- 3-svg-game/scripts.js
+++ 3-svg-game/scripts.js
@@ -11,19 +11,73 @@ function dropChip(column) {
         if (currentSpot == 'white') {
             if (turn) {
                 $('#c' + column + 'r' + row).attr("fill", "red");
+                checkForWinner("red");
                 turn = false;
                 break;
             } else {
                 $('#c' + column + 'r' + row).attr("fill", "yellow");
+                checkForWinner("yellow");
                 turn = true;
                 break;
             }
             
             
         }
+
     }
 
 }
 
-chips = []
+function checkForWinnerInColumn(color, column) {
+    if ($('#c' + column + 'r5').attr("fill") == color && 
+        $('#c' + column + 'r4').attr("fill") == color &&
+        $('#c' + column + 'r3').attr("fill") == color &&  
+        $('#c' + column + 'r2').attr("fill") == color) {
+        return true;
+        
+    }
+    if ($('#c' + column + 'r4').attr("fill") == color && 
+        $('#c' + column + 'r3').attr("fill") == color &&
+        $('#c' + column + 'r2').attr("fill") == color &&  
+        $('#c' + column + 'r1').attr("fill") == color) {
+        return true;
+        
+    }
+    if ($('#c' + column + 'r3').attr("fill") == color && 
+        $('#c' + column + 'r2').attr("fill") == color &&
+        $('#c' + column + 'r1').attr("fill") == color &&  
+        $('#c' + column + 'r0').attr("fill") == color) {
+        return true;
+        
+    }
+    return false;
+
+}
+function checkForWinnerinRow(color, row) {
+    if ($('#c6r' + row).attr("fill") == color && 
+        $('#c5r' + row).attr("fill") == color &&
+        $('#c4r' + row).attr("fill") == color &&  
+        $('#c3r' + row).attr("fill") == color) {
+        return true;
+        
+    }
+    return false;
+}
+
+function checkForWinner(color) {
+    for (let column = 0; column < 7; column++) {
+        if (checkForWinnerInColumn(color, column)) {
+            console.log("You won in col");
+            break;
+        }
+    }
+    for (let row = 0; row < 6; row++) {
+        if (checkForWinnerinRow(color, row)) {
+            console.log("You won in row");
+            break;
+        }
+    }
+}
+
+
 
