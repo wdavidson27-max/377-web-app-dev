38b7aad92f73b950f0393d31d8c29ca949a55bf7
Will <wdavidson27@hanoverstudents.org>
2025-10-23 12:46:39 -0400

    idk

--- 3-svg-game/scripts.js
+++ 3-svg-game/scripts.js
@@ -61,8 +61,77 @@ function checkForWinnerinRow(color, row) {
         return true;
         
     }
+    if ($('#c5r' + row).attr("fill") == color && 
+        $('#c4r' + row).attr("fill") == color &&
+        $('#c3r' + row).attr("fill") == color &&  
+        $('#c2r' + row).attr("fill") == color) {
+        return true;
+        
+    }
+    if ($('#c4r' + row).attr("fill") == color && 
+        $('#c3r' + row).attr("fill") == color &&
+        $('#c2r' + row).attr("fill") == color &&  
+        $('#c1r' + row).attr("fill") == color) {
+        return true;
+        
+    }
+    if ($('#c3r' + row).attr("fill") == color && 
+        $('#c2r' + row).attr("fill") == color &&
+        $('#c1r' + row).attr("fill") == color &&  
+        $('#c0r' + row).attr("fill") == color) {
+        return true;
+        
+    }
     return false;
 }
+
+function checkForWinnerDiagonalUR(color) {
+
+    if ($('#c0r0').attr("fill") == color && $('#c1r1').attr("fill") == color && $('#c2r2').attr("fill") == color && $('#c3r3').attr("fill") == color ||
+        ($('#c0r1').attr("fill") == color && $('#c1r2').attr("fill") == color && $('#c2r3').attr("fill") == color && $('#c3r4').attr("fill") == color) ||
+        ($('#c0r2').attr("fill") == color && $('#c1r3').attr("fill") == color && $('#c2r4').attr("fill") == color && $('#c3r5').attr("fill") == color) ||
+        ($('#c0r0').attr("fill") == color && $('#c1r1').attr("fill") == color && $('#c2r2').attr("fill") == color && $('#c3r3').attr("fill") == color) ||
+        ($('#c0r1').attr("fill") == color && $('#c1r2').attr("fill") == color && $('#c2r3').attr("fill") == color && $('#c3r4').attr("fill") == color) ||
+        ($('#c0r2').attr("fill") == color && $('#c1r3').attr("fill") == color && $('#c2r4').attr("fill") == color && $('#c3r5').attr("fill") == color) ||
+        ($('#c0r0').attr("fill") == color && $('#c1r1').attr("fill") == color && $('#c2r2').attr("fill") == color && $('#c3r3').attr("fill") == color) ||
+        ($('#c0r1').attr("fill") == color && $('#c1r2').attr("fill") == color && $('#c2r3').attr("fill") == color && $('#c3r4').attr("fill") == color) ||
+        ($('#c0r2').attr("fill") == color && $('#c1r3').attr("fill") == color && $('#c2r4').attr("fill") == color && $('#c3r5').attr("fill") == color) ||
+        ($('#c0r0').attr("fill") == color && $('#c1r1').attr("fill") == color && $('#c2r2').attr("fill") == color && $('#c3r3').attr("fill") == color) ||
+        ($('#c0r1').attr("fill") == color && $('#c1r2').attr("fill") == color && $('#c2r3').attr("fill") == color && $('#c3r4').attr("fill") == color) ||
+        ($('#c0r2').attr("fill") == color && $('#c1r3').attr("fill") == color && $('#c2r4').attr("fill") == color && $('#c3r5').attr("fill") == color) ) {
+        return true;
+
+        }
+        return false;
+    }   
+
+
+    //if ($('#c0r0' && '#c1r1' && '#c2r2' && '#c3r3').attr("fill") == color ||
+    //    $('#c0r1' && '#c1r2' && '#c2r3' && '#c3r4').attr("fill") == color ||
+    //    $('#c0r2' && '#c1r3' && '#c2r4' && '#c3r5').attr("fill") == color ||
+    //    $('#c1r0' && '#c2r1' && '#c3r2' && '#c4r3').attr("fill") == color ||
+    //    $('#c1r1' && '#c2r2' && '#c3r3' && '#c4r4').attr("fill") == color ||
+    //    $('#c1r2' && '#c2r3' && '#c3r4' && '#c4r5').attr("fill") == color ||
+    //    $('#c2r0' && '#c3r1' && '#c4r2' && '#c5r3').attr("fill") == color ||
+    //    $('#c2r1' && '#c3r2' && '#c4r3' && '#c5r4').attr("fill") == color ||
+    //    $('#c2r2' && '#c3r3' && '#c4r4' && '#c5r5').attr("fill") == color ||
+    //    $('#c3r0' && '#c4r1' && '#c5r2' && '#c6r3').attr("fill") == color ||
+    //    $('#c3r1' && '#c4r2' && '#c5r3' && '#c6r4').attr("fill") == color ||
+    //    $('#c3r2' && '#c4r3' && '#c5r4' && '#c6r5').attr("fill") == color) {
+    //    return true;
+    //
+    //}
+    //return false;    
+//}
+
+//function checkForWinnerDiagonalUL(color) {
+    //if ($('#c6r0' && '#c5r1' && '#c4r2' && '#c3r3').attr("fill") == color) {
+        //return true;
+
+    //}
+    //return false;
+//}
+
 
 function checkForWinner(color) {
     for (let column = 0; column < 7; column++) {
@@ -76,7 +145,13 @@ function checkForWinner(color) {
             console.log("You won in row");
             break;
         }
+    } 
+    if (checkForWinnerDiagonalUR(color)) {
+        console.log("You win Diagonal to Right");
     }
+    //if (checkForWinnerDiagonalUL(color)) {
+       // console.log("You win Diagonal to Left")
+   // }
 }
 
 
