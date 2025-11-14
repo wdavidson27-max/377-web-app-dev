444756932e9d1b233b5089cc1c3775a002dc80a5
Will <wdavidson27@hanoverstudents.org>
2025-11-14 12:45:49 -0500

    day 5

---
+++ AOC-2022/day5.py
@@ -0,0 +1,33 @@
+file = open("day5.txt", "r")
+lines = file.readlines()
+
+stacks = [
+    ['G', 'D', 'V', 'Z', 'J', 'S', 'B'],
+    ['Z', 'S', 'M', 'G', 'V', 'P'],
+    ['C', 'L', 'B', 'S', 'W', 'T', 'Q', 'F'],
+    ['H', 'J', 'G', 'W', 'M', 'R', 'V', 'Q'],
+    ['C', 'L', 'S', 'N', 'F', 'M', 'D'],
+    ['R', 'G', 'C', 'D'],
+    ['H', 'G', 'T', 'R', 'J', 'D', 'S', 'Q'],
+    ['P', 'F', 'V'],
+    ['D', 'R', 'S', 'T', 'J'],
+]
+
+
+for line in lines:
+    line = line.strip()
+
+    words = line.split(' ')
+
+    num_blocks = int(words[1])
+    source = int(words[3]) - 1
+    destination = int(words[5]) - 1
+    
+    for i in range(num_blocks):
+        block = stacks[source].pop()
+        stacks[destination].append(block)
+
+for i in range(len(stacks)):
+    print(stacks[i][-1], end='')
+
+print()
