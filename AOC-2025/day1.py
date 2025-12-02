e180fe68ffdc6f722f539190e7fe47bc2fbc9600
Will <wdavidson27@hanoverstudents.org>
2025-12-02 12:47:01 -0500

    day 2

---
+++ AOC-2025/day1.py
@@ -0,0 +1,5 @@
+file = open("day1.txt", "r")
+lines = file.readlines()
+
+for line in lines:
+    line = line.strip()
---
+++ AOC-2025/day2.py
@@ -0,0 +1,28 @@
+FILE = 'day2.txt'
+
+file = open(FILE, 'r')
+ranges = file.readlines()[0].split(',')
+
+total_1 = 0
+total_2 = 0
+
+for the_range in ranges:
+    start, end = (int(x) for x in the_range.split('-'))
+    for n in range(start, end + 1):
+        number = str(n)
+        
+        length = len(number)
+
+        if number[:len(number) // 2] == number[len(number) // 2:]:
+            total_1 += n
+
+        for l in range(1, length):
+            if length % l == 0:
+                chunks = [number[i:i+l] for i in range(0, len(number), l)]
+                if len(set(chunks)) == 1:
+                    total_2 += n
+                    break
+
+
+print('Part 1: ' + str(total_1))
+print('Part 2: ' + str(total_2))
