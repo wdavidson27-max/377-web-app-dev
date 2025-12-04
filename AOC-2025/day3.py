bb7ffa5808fd6730c4582ce43efe6ec6daa99f2a
Will <wdavidson27@hanoverstudents.org>
2025-12-04 12:46:13 -0500

    67

--- AOC-2025/day3.py
+++ AOC-2025/day3.py
@@ -1,7 +1,7 @@
 file = open("day3.txt", "r")
 lines = file.readlines()
 
-
+total = 0
 
 for line in lines:
     line = line.strip()
@@ -11,20 +11,22 @@ for line in lines:
     max = 0
     max2 = 0
     pos1 = 0
-    for i in range(len(numbers)):
+
+    for i in range(len(numbers) - 1):
         if max < numbers[i]:
             max = numbers[i]
             pos1 = i
-    
+            
 
-    for j in range(pos1+ 1):
+    for j in range(pos1 + 1,(len(numbers))):
          if max2 < numbers[j]:
              max2 = numbers[j]
              pos2 = j
-             
+            
     
+    total += max * 10 + max2
+print(total)
     
-    
-    print(max)  
-    print(max2)
+
+
              
