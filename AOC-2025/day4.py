08f4fcb6687a05a0c2adcede3e0962f0cdc54603
Will <wdavidson27@hanoverstudents.org>
2025-12-09 12:48:22 -0500

    66

--- AOC-2025/day4.py
+++ AOC-2025/day4.py
@@ -2,17 +2,29 @@ file = open("day4.txt", "r")
 lines = file.readlines()
 
 grid = []
+
 for line in lines:
     line = line.strip()
 
     grid.append([x for x in line])
 
-for row in line:
-    for column in line:
-
+        
 
 print(grid)   
 print(grid[0]) 
 print(grid[0][2])
-    
 
+
+# We have to make a nested loop to go through the rows and 
+# columns in the grid so we can see if a roll of paper can
+# be accessed. Then we can keep all of the rolls that can be
+# accessed in a variable called total and then at the end print
+# out the total 
+
+total = 0
+
+for row in grid:
+    for column in row:
+        if column == '@' and :
+            total += 1
+print(total)
---
+++ AOC-2025/day9.py
@@ -0,0 +1,5 @@
+file = open("day9.txt", "r")
+lines = file.readlines()
+
+for line in lines:
+    line = line.strip()
